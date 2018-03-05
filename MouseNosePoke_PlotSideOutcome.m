2b4d4f9c1f7f7340f5f7da46245471220f4009dc
marionbosc <bosc@cshl.edu>
2018-03-05 09:51:21 -0500

    Name change

---
+++ MouseNosePoke_PlotSideOutcome.m
@@ -0,0 +1,72 @@
+function MouseNosePoke_PlotSideOutcome(AxesHandle, Action, varargin)
+global nTrialsToShow %this is for convenience
+global BpodSystem
+
+switch Action
+    case 'init'
+        %initialize pokes plot
+        nTrialsToShow = 90; %default number of trials to display
+        
+        if nargin >= 3 %custom number of trials
+            nTrialsToShow =varargin{1};
+        end
+        axes(AxesHandle);
+        %plot in specified axes
+        BpodSystem.GUIHandles.CurrentTrialCircle = line(-1,0.5, 'LineStyle','none','Marker','o','MarkerEdge','k','MarkerFace',[1 1 1], 'MarkerSize',6);
+        BpodSystem.GUIHandles.CurrentTrialCross = line(-1,0.5, 'LineStyle','none','Marker','+','MarkerEdge','k','MarkerFace',[1 1 1], 'MarkerSize',6);
+        BpodSystem.GUIHandles.RewardedL = line(-1,1, 'LineStyle','none','Marker','o','MarkerEdge','g','MarkerFace','g', 'MarkerSize',6);
+        BpodSystem.GUIHandles.RewardedR = line(-1,0, 'LineStyle','none','Marker','o','MarkerEdge','g','MarkerFace','g', 'MarkerSize',6);
+        BpodSystem.GUIHandles.EarlyWithdrawal = line(-1,0, 'LineStyle','none','Marker','d','MarkerEdge','none','MarkerFace','b', 'MarkerSize',6);
+        BpodSystem.GUIHandles.Jackpot = line(-1,0, 'LineStyle','none','Marker','x','MarkerEdge','r','MarkerFace','r', 'MarkerSize',7);
+        set(AxesHandle,'TickDir', 'out','YLim', [-1, 2],'XLim',[0,nTrialsToShow], 'YTick', [0 1],'YTickLabel', {'Right','Left'}, 'FontSize', 16);
+        xlabel(AxesHandle, 'Trial#', 'FontSize', 18);
+        hold(AxesHandle, 'on');
+        
+    case 'update'
+        CurrentTrial = varargin{1};
+        ChoiceLeft = BpodSystem.Data.Custom.ChoiceLeft;
+        
+        % recompute xlim
+        [mn, ~] = rescaleX(AxesHandle,CurrentTrial,nTrialsToShow);
+        
+        set(BpodSystem.GUIHandles.CurrentTrialCircle, 'xdata', CurrentTrial, 'ydata', .5);
+        set(BpodSystem.GUIHandles.CurrentTrialCross, 'xdata', CurrentTrial, 'ydata', .5);
+        
+        %Plot past trials
+        if ~isempty(ChoiceLeft)
+            indxToPlot = mn:CurrentTrial-1;
+            %Plot Rewarded Left
+            ndxRwdL = ChoiceLeft(indxToPlot) == 1;
+            Xdata = indxToPlot(ndxRwdL); Ydata = ones(1,sum(ndxRwdL));
+            set(BpodSystem.GUIHandles.RewardedL, 'xdata', Xdata, 'ydata', Ydata);
+            %Plot Rewarded Right
+            ndxRwdR = ChoiceLeft(indxToPlot) == 0;
+            Xdata = indxToPlot(ndxRwdR); Ydata = zeros(1,sum(ndxRwdR));
+            set(BpodSystem.GUIHandles.RewardedR, 'xdata', Xdata, 'ydata', Ydata);            
+        end
+        if ~isempty(BpodSystem.Data.Custom.EarlyWithdrawal)
+            indxToPlot = mn:CurrentTrial-1;
+            ndxEarly = BpodSystem.Data.Custom.EarlyWithdrawal(indxToPlot);
+            XData = indxToPlot(ndxEarly);
+            YData = 0.5*ones(1,sum(ndxEarly));
+            set(BpodSystem.GUIHandles.EarlyWithdrawal, 'xdata', XData, 'ydata', YData);
+        end
+        if ~isempty(BpodSystem.Data.Custom.Jackpot)
+            indxToPlot = mn:CurrentTrial-1;
+            ndxJackpot = BpodSystem.Data.Custom.Jackpot(indxToPlot);
+            XData = indxToPlot(ndxJackpot);
+            YData = 0.5*ones(1,sum(ndxJackpot));
+            set(BpodSystem.GUIHandles.Jackpot, 'xdata', XData, 'ydata', YData);
+        end
+
+end
+
+end
+
+function [mn,mx] = rescaleX(AxesHandle,CurrentTrial,nTrialsToShow)
+FractionWindowStickpoint = .75; % After this fraction of visible trials, the trial position in the window "sticks" and the window begins to slide through trials.
+mn = max(round(CurrentTrial - FractionWindowStickpoint*nTrialsToShow),1);
+mx = mn + nTrialsToShow - 1;
+set(AxesHandle,'XLim',[mn-1 mx+1]);
+end
+
